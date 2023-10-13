6b8e15babf23dbc1e1f75b60b80af850f23cb0e1
Jeanne <jeannevoisin@j.vsn>
2023-10-13 16:42:05 +0200

    add clockwise and counterclockwise

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 // pin definitions
 int stepper(int swpulse, int m0, int m1, int m2, int dir, int dur);
+void clockwise(void);
+void counterclockwise(void);
 DigitalOut STBY(D8);
 DigitalOut STEP(D7);
 DigitalOut DIr(D6);
@@ -12,35 +14,51 @@ DigitalOut M2(D9);
 
 int main()
 {
-    STBY = 0;
-    EN = 0;
+    STBY = 1;
+    EN = 1;
     M0 = 0;
-    M1 = 1;
+    M1 = 0;
     M2 = 0;
 
     ThisThread::sleep_for(1s);
     while (1)
     {
-        stepper(1000, 1, 0, 0, 0, 1);
+        printf("clockwise\n");
+        clockwise();
+        ThisThread::sleep_for(1s);
+        printf("counterclockwise\n");
+        counterclockwise();
+        ThisThread::sleep_for(1s);
     }
+    return 0;
 }
 
 //  function stepper for EVALSP820-XS
 int stepper(int swpulse, int m0, int m1, int m2, int dir, int dur)
 {
-    MO=m0;
-    M1=m1;
-    M2=m2;
-    DIr=dir;
+    M0 = m0;
+    M1 = m1;
+    M2 = m2;
+    DIr = dir;
     EN = 1;
     // STEP GENERATOR
     for (int i = 0; i < swpulse; i++)
     {
         STEP = 1;
-        ThisThread::sleep_for(100ms);
+        ThisThread::sleep_for(1ms);
         STEP = 0;
-        ThisThread::sleep_for(100ms);
+        ThisThread::sleep_for(1ms);
     }
-    EN = 0; // output stage off
+    EN = 0;
     return 0;
+}
+
+void clockwise(void)
+{
+    stepper(1000, 0, 0, 0, 1, 1);
+}
+
+void counterclockwise(void)
+{
+    stepper(1000, 0, 0, 0, 0, 1);
 }
